17574852ca9bd967d30a72d96f1eda74539eae55
Nasuri <victorbenedict.bulaong@gmail.com>
2023-07-12 20:13:17 +0800

    Add modules

---
+++ storage.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/storage.js ***!
+  \************************/
+  //This module handles user's local save data, providing methods for storing and retrieving data.
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3RvcmFnZS5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSxrRyIsInNvdXJjZXMiOlsid2VicGFjazovL3Byb2plY3QtdG9kby1saXN0Ly4vc3JjL3N0b3JhZ2UuanMiXSwic291cmNlc0NvbnRlbnQiOlsiICAvL1RoaXMgbW9kdWxlIGhhbmRsZXMgdXNlcidzIGxvY2FsIHNhdmUgZGF0YSwgcHJvdmlkaW5nIG1ldGhvZHMgZm9yIHN0b3JpbmcgYW5kIHJldHJpZXZpbmcgZGF0YS4iXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
