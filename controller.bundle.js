b3fb28d0692f2b995f9173be6b00058f2ef06b76
Nasuri <victorbenedict.bulaong@gmail.com>
2023-07-14 10:28:48 +0800

    Modify modules

--- controller.bundle.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!***************************!*\
-  !*** ./src/controller.js ***!
-  \***************************/
-//This module acts as the central coordinator, handling the communication and coordination between the logic, display, and storage modules.
-/******/ })()
-;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29udHJvbGxlci5idW5kbGUuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7QUFBQSwySSIsInNvdXJjZXMiOlsid2VicGFjazovL3Byb2plY3QtdG9kby1saXN0Ly4vc3JjL2NvbnRyb2xsZXIuanMiXSwic291cmNlc0NvbnRlbnQiOlsiLy9UaGlzIG1vZHVsZSBhY3RzIGFzIHRoZSBjZW50cmFsIGNvb3JkaW5hdG9yLCBoYW5kbGluZyB0aGUgY29tbXVuaWNhdGlvbiBhbmQgY29vcmRpbmF0aW9uIGJldHdlZW4gdGhlIGxvZ2ljLCBkaXNwbGF5LCBhbmQgc3RvcmFnZSBtb2R1bGVzLiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
